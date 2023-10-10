98475319d3b4bc70aa2aa67df346c3760275c2b6
ms0753 <ms0753@naver.com>
2023-10-10 23:18:50 +0900

    [Update] 인벤토리 구현

--- Unity week3 3D/Assets/Scriptable Objects/Scripts/ItemData.cs
+++ Unity week3 3D/Assets/Scriptable Objects/Scripts/ItemData.cs
@@ -17,6 +17,13 @@ public enum ConsumableType
     Health
 }
 
+[System.Serializable]
+public class ItemDataConsumable
+{
+    public ConsumableType type;
+    public float value;
+}
+
 [CreateAssetMenu(fileName = "Item", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
@@ -30,4 +37,7 @@ public class ItemData : ScriptableObject
     [Header("Stacking")]
     public bool canStack;
     public int maxStackAmount;
+
+    [Header("Consumable")]
+    public ItemDataConsumable[] consumables;
 }
